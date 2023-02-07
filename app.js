0406639dd53e71ded17ba565c4fdd2922e43f412
Ron Barzilay <ron.barzilay@gmail.com>
2023-02-07 23:10:52 +0200

    Fetching data from url by app.get('/url', function(req, res){ serial = req.query.serial; res.send('serial: ' + serial); });

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const express = require("express");
 const app = express();
 
 app.listen(3000);
+
+app.get("/", function (req, res) {
+  res.send("Ron did it");
+});
+
+// app.get("/phones", function (req, res) {
+//   res.send("Which phone do you want to get?");
+// });
+
+app.get("/phones/:serial", function (req, res) {
+  serial = req.params.serial;
+  res.send("Here goes Phone no. " + serial);
+});
+
+app.get("/phones", function (req, res) {
+  serial = req.query.serial;
+  res.send("Here goes Phone no. " + serial);
+});
